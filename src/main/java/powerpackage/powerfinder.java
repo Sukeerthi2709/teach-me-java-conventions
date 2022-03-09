53ef2203e0a383e5dd1f5a5082693a5d8d28df00
SUKEERTHI PITTA <61060077+Sukeerthi2709@users.noreply.github.com>
2022-03-09 10:38:06 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,7 +1,7 @@
 package powerpackage;
 
 public class PowerFinder {
-    public static int OF(int value, int pow) {
+    public static int of(int value, int pow) {
         int result = 1;
         for (int i = 0; i<pow; i++) {
             result*=value;
